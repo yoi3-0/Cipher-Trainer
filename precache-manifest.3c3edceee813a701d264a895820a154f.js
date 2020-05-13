ffc3f2688f3531bd0a89555891bbf116a04319c5
yoi3.0 <dkurpachenko@gmail.com>
2020-05-13 12:40:41 +0300

    Updates

---
+++ precache-manifest.3c3edceee813a701d264a895820a154f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96a758c18cecd5df506",
+    "url": "/Cipher-Trainer/static/js/runtime~main.e2da6144.js"
+  },
+  {
+    "revision": "cae61ac0edb8c4c25999",
+    "url": "/Cipher-Trainer/static/js/main.c9797d5c.chunk.js"
+  },
+  {
+    "revision": "316a207324ad72c6978f",
+    "url": "/Cipher-Trainer/static/js/2.2429e3df.chunk.js"
+  },
+  {
+    "revision": "cae61ac0edb8c4c25999",
+    "url": "/Cipher-Trainer/static/css/main.33af87b0.chunk.css"
+  },
+  {
+    "revision": "316a207324ad72c6978f",
+    "url": "/Cipher-Trainer/static/css/2.bd12921c.chunk.css"
+  },
+  {
+    "revision": "e195bd58c0020b2205bdfbbb7ebc5ad2",
+    "url": "/Cipher-Trainer/index.html"
+  }
+];
