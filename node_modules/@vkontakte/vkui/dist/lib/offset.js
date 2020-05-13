abc833d23979c3cb19471e89278c5c9195c088d2
yoi3.0 <dkurpachenko@gmail.com>
2020-05-13 13:30:59 +0300

    Remove the now ignored directory node_modules

---
+++ node_modules/@vkontakte/vkui/dist/lib/offset.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getOffsetRect = getOffsetRect;
+
+function getOffsetRect(elem) {
+  var box = elem.getBoundingClientRect();
+  var body = document.body;
+  var doc = document.documentElement;
+  var scrollTop = window.pageYOffset || doc.scrollTop || body.scrollTop;
+  var scrollLeft = window.pageXOffset || doc.scrollLeft || body.scrollLeft;
+  var clientTop = doc.clientTop || body.clientTop || 0;
+  var clientLeft = doc.clientLeft || body.clientLeft || 0;
+  return {
+    top: box.top + scrollTop - clientTop,
+    left: box.left + scrollLeft - clientLeft,
+    width: elem.offsetWidth,
+    height: elem.offsetHeight
+  };
+}
+//# sourceMappingURL=offset.js.map
